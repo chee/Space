ac050f58fa9e35b8affcccb62262419f5e6283ee
chee <yay@chee.party>
2022-08-11 21:56:37 -0500

    Initial Commit

---
+++ Space/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Space
+//
+//  Created by chee on 2022-08-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Space/SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceApp.swift
+//  Space
+//
+//  Created by chee on 2022-08-11.
+//
+
+import SwiftUI
+
+@main
+struct SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
