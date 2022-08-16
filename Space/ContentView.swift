e0363cef810aabf0c5d20b9f03ced33569573542
chee <yay@chee.party>
2022-08-16 03:47:36 -0500

    add ql pane

--- Space/ContentView.swift
+++ Space/ContentView.swift
@@ -8,71 +8,101 @@
 import SwiftUI
 import AppKit
 import UniformTypeIdentifiers
+import Quartz
+
+struct QLImage: NSViewRepresentable {
+    var url: URL
+    
+    func makeNSView(context: NSViewRepresentableContext<QLImage>) -> QLPreviewView {
+        let preview = QLPreviewView(frame: .zero, style: .normal)
+        preview?.autostarts = true
+        preview?.previewItem = url as QLPreviewItem
+        
+        return preview ?? QLPreviewView()
+    }
+    
+    func updateNSView(_ nsView: QLPreviewView, context: NSViewRepresentableContext<QLImage>) {
+        nsView.previewItem = url as QLPreviewItem
+    }
+    
+    typealias NSViewType = QLPreviewView
+}
+
 
 let ws = NSWorkspace.shared
 let fm = FileManager.default
-let root = URL(fileURLWithPath: "/Users/chee/Documents/Notebooks/").resolvingSymlinksInPath()
+let root = URL(
+    fileURLWithPath: "/Users/chee/Documents/Notebooks/"
+).resolvingSymlinksInPath()
 
-struct FileItem: Hashable, Identifiable, CustomStringConvertible {
-    var id: Self { self }
-    var url: URL
-    // no point being lazy right now, outlinegroup isn't lazy
-    //    lazy var children: [FileItem] = {
-    //           return []
-    //    }()
-    var children: [FileItem]?
-    var icon: NSImage {
-        if (children != nil) {
-            return ws.icon(for: UTType.folder)
+
+struct SidebarFileView: View {
+    var file: FileItem
+    @State var isExpanded: Bool = false
+
+    var body: some View {
+        if file.isFolder {
+            DisclosureGroup(
+                isExpanded: $isExpanded,
+                content: {
+                    if isExpanded {
+                        ForEach(getChildren(url: file.url), id: \.self.id) { childNode in
+                            SidebarFileView(file: childNode)
+                        }
+                    }
+                },
+                label: {
+                    Image(nsImage: file.icon)
+                    Text(" " + file.name)
+                })
         }
-        return ws.icon(
-            for: UTType(filenameExtension: url.pathExtension)
-            ?? UTType.content
-        )
     }
-    var description: String {
-        return url.lastPathComponent
-    }
+
 }
 
-func getTableContextFromSidebar(set: Set<FileItem>) -> [FileItem] {
-    if set.count != 1 {
-        return []
+struct FileItem: Identifiable, Hashable, Equatable, Comparable {
+    static func < (lhs: FileItem, rhs: FileItem) -> Bool {
+        return lhs.name < rhs.name
     }
-    if set.first!.children == nil {
-        return []
+    
+    var id: Self {self}
+    var url: URL
+    
+    lazy var children = {
+       return getChildren(url: url).sorted()
+    }()
+
+    var isFolder: Bool { self.type == UTType.folder }
+    var icon: NSImage
+    var type: UTType
+    var name: String
+    
+    init(url: URL, type: UTType?) {
+        self.url = url
+        self.icon = ws.icon(for: type ?? UTType.content)
+        self.name = url.lastPathComponent
+        self.type = type ?? UTType.content
+        
     }
-    return set.first!.children!
 }
-//
-//func getPreviewContextFromTable(set: Set<FileItem>) -> String {
-//    if set.count != 1 {
-//        return ""
-//    }
-//    if set.first!.content == nil {
-//        return ""
-//    }
-//    return set.first!.content!
-//}
 
 func getChildren(url: URL) -> [FileItem] {
     do {
         let resourceKeys = Array<URLResourceKey>([
-            .isDirectoryKey
+            .contentTypeKey,
         ])
         var children: [FileItem] = []
         for fileUrl in try fm.contentsOfDirectory(at: url, includingPropertiesForKeys: resourceKeys) {
             guard let values = try?
                     fileUrl.resourceValues(forKeys: Set<URLResourceKey>([
-                        .isDirectoryKey
+                        .contentTypeKey
                     ])),
-                  let isDirectory = values.isDirectory
+                  let contentType = values.contentType
             else {continue}
-            var file = FileItem(url: fileUrl)
-            if isDirectory {
-                file.children = getChildren(url: fileUrl)
-            }
-            children.append(file)
+            children.append(FileItem(
+                url: fileUrl,
+                type: contentType
+            ))
         }
         return children
     } catch {
@@ -80,57 +110,50 @@ func getChildren(url: URL) -> [FileItem] {
     }
 }
 
-func getFiles() -> [FileItem] {
-    return getChildren(url: root)
-}
 
 struct ContentView: View {
-    @State private var sidebarSelection = Set<FileItem>()
-    @State private var sidebarTarget: FileItem?
+    @State var sidebarSelection = Set<FileItem>()
+    @State var expansion: Set<UUID> = []
+    @State var sidebarTarget: [FileItem]?
     @State private var tableSelection = Set<FileItem>()
     @State private var tableTarget: FileItem?
     @State private var previewText: String = ""
-    let files = getFiles()
-    
+    let dirs = [FileItem(
+        url: root,
+        type: UTType.folder // custom icons?
+    )].sorted()
+
     var body: some View {
         HSplitView {
-            List(selection: $sidebarSelection) {
-                OutlineGroup(files, children: \.children) {item in
-                    Image(nsImage: item.icon)
-                    Text(item.description)
-                }
+            List(dirs, selection: $sidebarSelection) {file in
+                SidebarFileView(file: file, isExpanded: true)
             }
-            .listStyle(.sidebar)
             .onChange(of: sidebarSelection) {selection in
                 tableSelection.removeAll()
                 sidebarTarget = nil
                 if (selection.count == 1) {
-                    sidebarTarget = sidebarSelection.first!
+                    var target = selection.first!
+                    sidebarTarget = target.children
                 }
             }
-        VSplitView {
-            Table(getTableContextFromSidebar(set: sidebarSelection), selection: $tableSelection) {
-                TableColumn("name", value: \.url.lastPathComponent)
-                TableColumn("path", value: \.url.path)
-                }.onChange(of: tableSelection) {tableSelection in
-                    tableTarget = nil
-                    if (tableSelection.count == 1) {
-                        tableTarget = tableSelection.first!
-                    }
-                }
-                TextEditor(text: $previewText)
-                    .onChange(of: tableTarget) {target in
-                        if (target != nil) {
-                            let contents = fm.contents(atPath: target!.url.path)
-                            if (contents != nil) {
-                                previewText = String(
-                                    decoding: contents!,
-                                    as: UTF8.self
-                                )
-                            }
+            .frame(idealWidth: 10, alignment: .leading)
+            VSplitView {
+                if sidebarTarget != nil {
+                    Table(sidebarTarget!, selection: $tableSelection) {
+                        TableColumn("name", value: \.url.lastPathComponent)
+                        TableColumn("path", value: \.url.path)
+                    }.onChange(of: tableSelection) {tableSelection in
+                        tableTarget = nil
+                        if (tableSelection.count == 1) {
+                            tableTarget = tableSelection.first!
                         }
                     }
+                }
+                if tableTarget != nil {
+                    QLImage(url:tableTarget!.url)
+                }
             }
+            .frame(minWidth: 400, alignment: .trailing)
         }
     }
 }
